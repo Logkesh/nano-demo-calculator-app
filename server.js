898b264dbb1905a1970ca1bbeb6658adb62e1eea
Logkesh <logkeshhemanth@gmail.com>
2023-08-15 21:10:10 +0530

    again!!

--- server.js
+++ server.js
@@ -14,23 +14,16 @@ calculatorRouter.get('/greeting', (req, res) => {
 });
 
 calculatorRouter.post('/add', (req, res) => {
-    const num1 = Number(req.body.num1);
-    const num2 = Number(req.body.num2);
+    const num1 = Number(req.body.first);
+    const num2 = Number(req.body.second);
 
-    if (isNaN(num1) || isNaN(num2)) {
-        return res.status(400).send('Invalid input. Please provide valid numbers.');
-    }
     const result = num1 + num2;
     res.send(`Addition result: ${result}`);
 });
 
 calculatorRouter.post('/subtract', (req, res) => {
-    const num1 = Number(req.body.num1);
-    const num2 = Number(req.body.num2);
-
-    if (isNaN(num1) || isNaN(num2)) {
-        return res.status(400).send('Invalid input. Please provide valid numbers.');
-    }
+    const num1 = Number(req.body.first);
+    const num2 = Number(req.body.second);
 
     const result = num1 - num2;
     res.send(`Subtraction result: ${result}`);
